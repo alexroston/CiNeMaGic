e8cf8035d28213059395930f98183682c4df9293
fxj9a <adrian.sprucer@gmail.com>
2018-06-22 22:24:33 +0200

    _ pam anderson is going to love me.

---
+++ CinemagiC/CinemagiC/Services/EmptyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CinemagiC.Services
+{
+    public class HttpRequest
+    {
+        public HttpRequest()
+        {
+            string jsonString = string.Empty;
+            string url = @"https://api.themoviedb.org/3/movie/{movie_id}/lists?api_key=de2c61fd451b50de11cee234a5d8346b&language=en-US&page=1";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                jsonString = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(jsonString);
+        }
+    }
+}
