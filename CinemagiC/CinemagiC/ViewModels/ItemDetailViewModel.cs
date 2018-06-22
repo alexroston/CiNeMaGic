e566dfe1899adba0a22019b02217b2b63c317aae
mkolbe <martin.kolbe@stud.htwk-leipzig.de>
2018-06-22 21:19:53 +0200

    Initial commit

---
+++ CinemagiC/CinemagiC/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CinemagiC.Models;
+
+namespace CinemagiC.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
