749b6cf573b8f6c6dcac0bbc055278d04a546c3d
louis <dearnleyml@aol.com>
2018-06-22 22:18:01 +0200

    hello world :D

---
+++ CinemagiC/CinemagiC/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemagiC.Models
+{
+    class Movie
+    {
+        public String title { get; set; }
+        public String id { get; set; }
+        public String name { get; set; }
+    }
+}
--- CinemagiC/CinemagiC/Views/MainPage.xaml.cs
+++ CinemagiC/CinemagiC/Views/MainPage.xaml.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using CinemagiC.Models;
+using Newtonsoft.Json;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CinemagiC.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
-	{
-		public MainPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            string json = @"{
+                           'name': 'Bad Boys',
+                            'ReleaseDate': '1995-4-7T00:00:00',
+                            'Genres': [
+                                'Action',
+                                'Comedy'
+                              ]
+                            }";
+
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+        }
+    }
 }
